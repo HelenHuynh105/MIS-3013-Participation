a82c6959fa633a914bd84069fd24a5ff3ee77e85
Helen <Helen.Huynh-1@ou.edu>
2021-02-12 15:54:20 -1200

    Coin Toss

---
+++ CoinToss/CoinToss/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            const string name = "Helen Huynh";
+            string guess;
+            int randomNumber = rand.Next(1,3);
+
+
+            Console.WriteLine("Guess heads or tails>>");
+            guess = Console.ReadLine();
+
+            if (guess == "heads" & randomNumber == 1)
+            {
+                
+                Console.WriteLine("The coin landed on " + randomNumber.ToString("Heads") + "! Your guess was right");
+            }
+
+            else if (guess == "heads" & randomNumber == 2)
+            {
+                Console.WriteLine("The coin landed on " + randomNumber.ToString("Tails") + "! Your guess was wrong");
+            }
+
+            else if (guess == "tails" & randomNumber == 2)
+            {
+                Console.WriteLine("The coin landed on " + randomNumber.ToString("Tails") + "! Your guess was right");
+            }
+
+            else
+            {
+                Console.WriteLine("The coin landed on " + randomNumber.ToString("Heads") + "! Your guess was wrong");
+            }
+
+            Console.WriteLine(name);
+        }
+    }
+}
