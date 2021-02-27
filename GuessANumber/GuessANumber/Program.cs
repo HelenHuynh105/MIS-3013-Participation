e8625f4b956bfd267ac54a65cdbc055ba4c1c184
Helen <Helen.Huynh-1@ou.edu>
2021-02-26 15:10:16 -1200

    c

---
+++ GuessANumber/GuessANumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int guess;
+            string minNbr, maxNbr;
+            int luckyNbr;
+
+            Console.WriteLine("What is your minimum number>>");
+            minNbr = Console.ReadLine();
+
+            Console.WriteLine("What is your maximum number>>");
+            maxNbr = Console.ReadLine();
+
+            luckyNbr = rand.Next(minNbr, maxNbr);
+
+
+
+
+        }
+    }
+}
