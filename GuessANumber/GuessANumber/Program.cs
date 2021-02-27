e2d4f85a42157d21f0ba20884a419710da92098e
Helen <Helen.Huynh-1@ou.edu>
2021-02-26 15:42:46 -1200

    commit

--- GuessANumber/GuessANumber/Program.cs
+++ GuessANumber/GuessANumber/Program.cs
@@ -7,17 +7,58 @@ namespace GuessANumber
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int guess;
-            string minNbr, maxNbr;
-            int luckyNbr;
+            string answer, guess;
+            int minNbr, maxNbr;
+            int luckyNbr, correctNbr;
 
             Console.WriteLine("What is your minimum number>>");
-            minNbr = Console.ReadLine();
+            answer = Console.ReadLine();
+
+            while (int.TryParse(answer, out minNbr) == false)
+            {
+                Console.WriteLine($"Your answer {answer} is not valid. Please enter another number");
+                answer = Console.ReadLine();
+            }
 
             Console.WriteLine("What is your maximum number>>");
-            maxNbr = Console.ReadLine();
+            answer = Console.ReadLine();
+
+            while (int.TryParse(answer, out maxNbr) == false)
+            {
+                Console.WriteLine($"Your answer {answer} is not valid. Please enter another number");
+                answer = Console.ReadLine();
+            }
 
             luckyNbr = rand.Next(minNbr, maxNbr);
+
+            Console.WriteLine("Please enter your guess>>");
+            guess = Console.ReadLine();
+
+            while (int.TryParse(guess, out correctNbr) == false)
+            {
+                Console.WriteLine($"Your guess {guess} is not valid. Please enter another guess");
+                guess = Console.ReadLine();
+            }
+
+            while (int.TryParse(guess, out correctNbr) == true)
+            {
+                if (correctNbr == luckyNbr)
+                {
+                    Console.WriteLine($"Your answer was correct! The number was {luckyNbr}");
+                    Environment.Exit(-1);
+                }
+                else if (correctNbr > luckyNbr)
+                {
+                    Console.WriteLine($"Your answer was too high! Guess again>>");
+                    guess = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your guess was too low! Guess again>>");
+                    guess = Console.ReadLine();
+                }
+            }
+
 
 
 
