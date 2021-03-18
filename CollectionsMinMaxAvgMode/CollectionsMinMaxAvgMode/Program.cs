c5830bde59a26152777969290caf6ea06a62ce63
Helen <Helen.Huynh-1@ou.edu>
2021-03-18 10:25:35 -1200

    commit

---
+++ CollectionsMinMaxAvgMode/CollectionsMinMaxAvgMode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsMinMaxAvgMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
