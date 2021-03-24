887d7b93c4f7306728a0661544a405430981f486
Helen <Helen.Huynh-1@ou.edu>
2021-03-23 13:26:32 -1200

    commit

--- CollectionsMinMaxAvgMode/CollectionsMinMaxAvgMode/Program.cs
+++ CollectionsMinMaxAvgMode/CollectionsMinMaxAvgMode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CollectionsMinMaxAvgMode
 {
@@ -6,7 +7,70 @@ namespace CollectionsMinMaxAvgMode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<double> ExamGrades = new List<double>();
+            Dictionary<double, int> MODE = new Dictionary<double, int>();
+            string answer;
+
+            do
+            {
+                Console.WriteLine("What is one of your exam grades? >>");
+                double answers = Convert.ToDouble(Console.ReadLine());
+
+                ExamGrades.Add(answers);
+
+                Console.WriteLine("Do you have another exam grade?>>");
+                answer = Console.ReadLine();
+
+                if (MODE.ContainsKey(answers) == false)
+                {
+                    MODE.Add(answers, 1);
+                }
+                else
+                {
+                    MODE[answers] = MODE[answers] + 1;
+                }
+
+            } while (answer.ToLower()[0] == 'y');
+
+            double min = ExamGrades[0];
+            double max = ExamGrades[0];
+            double sum = 0;
+            int NumCount = 1;
+
+            foreach (var answers in ExamGrades)
+            {
+                if (answers < min)
+                {
+                    min = answers;
+                }
+
+                if (answers > max)
+                {
+                    max = answers;
+                }
+
+                sum += answers;
+               
+            }
+
+            foreach (double key in MODE.Keys)
+            {
+                double answers = key;
+                if (MODE[answers] > NumCount)
+                {
+                    NumCount++;
+                    Console.WriteLine($"The mode is {answers} x {NumCount}");
+                }
+            }
+
+            double average = sum / ExamGrades.Count;
+
+            Console.WriteLine($"Your minimum exam grade is {min.ToString("N2")}");
+            Console.WriteLine($"Your maximum exam grade is {max.ToString("N2")}");
+            Console.WriteLine($"Your average for all your exams is {average.ToString("N2")}");
+            
+
+
         }
     }
 }
