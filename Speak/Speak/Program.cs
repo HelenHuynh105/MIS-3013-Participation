f654bcc45d9a078a340e40dde620ef94fc137641
Helen <Helen.Huynh-1@ou.edu>
2021-04-09 14:11:02 -1200

    commit

---
+++ Speak/Speak/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which animal sound do you want?(Duck,Pig,Cow)");
+            string answer = Console.ReadLine();
+
+            Console.WriteLine($"{answer} makes this sound {AnimalSound(answer)}");
+        }
+
+        static string AnimalSound(string answer)
+        {
+            string sound = "";
+
+            if (answer.ToLower() == "Duck")
+            {
+                sound = "Quack";
+            }
+            if (answer.ToLower() == "Pig")
+            {
+                sound = "Oink";
+            }
+            if (answer.ToLower() == "Cow")
+            {
+                sound = "Moo";
+            }
+
+            return sound;
+        }
+    }
+}
