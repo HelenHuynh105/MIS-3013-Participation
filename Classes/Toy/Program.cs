aa57337d6a7f3b3b9097fe51305e44817d467ce1
Helen <Helen.Huynh-1@ou.edu>
2021-04-16 22:58:45 -0500

    commit

---
+++ Classes/Toy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Toy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toy item1 = new Toy();
+
+            item1.Manufacturer = "Mattle.INC";
+            item1.Name = "Birthday Wishes Barbie Doll";
+            item1.Price = 46.98;
+            item1.Notes = "'Barbie Birthday Wushes doll adds sweetness and fun to any birthday celebration' - Walmart";
+
+            string Item1Information = item1.GetAisle();
+
+            Console.WriteLine($"Item 1 - {Item1Information}");
+
+
+            Toy item2 = new Toy();
+            item2.Manufacturer = "Hasbro";
+            item2.Name = "Play-Doh 8 Color Rainbow Pack";
+            item2.Price = 4.94;
+            item2.Notes = "For Ages 2 and up";
+
+            string Item2Information = item2.GetAisle();
+
+            Console.WriteLine($"\nItem 2 - {Item2Information}");
+
+        }
+    }
+}
---
+++ Classes/Toy/Toy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toy
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Notes { get; set; }
+
+        public Toy()
+        {
+            Manufacturer = "Mattel";
+            Name = "Barbie Doll";
+            Price = 10.50;
+            Notes = "For ages 6 and up";
+        }
+
+        public string GetAisle()
+        {
+            Random rand = new Random();
+            int randomNum = rand.Next(1, 25);
+
+            return "\nManufacturer: " +Manufacturer.Substring(0,1) + randomNum 
+                + "\nName: " + Name + "\nPrice: " + Price.ToString("C") + "\nNotes: " + Notes;
+        }
+    }
+}
